19dcb3423b0464d7090049dca01c5b42c5d9b141
Daruuu <thedaruny16@gmail.com>
2023-09-29 12:42:45 +0200

    u int

--- ft_putuint_nbr.c
+++ ft_putuint_nbr.c
@@ -6,12 +6,11 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 12:58:13 by dasalaza          #+#    #+#             */
-/*   Updated: 2023/09/27 16:49:46 by dasalaza         ###   ########.fr       */
+/*   Updated: 2023/09/29 12:42:10 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
 /*
 static int    ft_put_u_nbr(unsigned int nbr)
 {
@@ -30,10 +29,17 @@ static int    ft_put_u_nbr(unsigned int nbr)
 	return (digit_count);
 }
 */
-
 void    ft_putuint_nbr(unsigned int nbr, size_t *counter_len)
 {
-
+	if (nbr >= 10)
+	{
+		ft_putuint_nbr(nbr / 10, counter_len);
+		if (*counter_len == -1)
+			return ;
+	}
+	ft_putchar(nbr % 10 + '0', counter_len);
+	if (*counter_len < 0)
+		return ;
 }
 /*
 unsigned int    ft_put_unsigned_nbr(unsigned int nbr, size_t counter_len)
