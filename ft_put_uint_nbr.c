6335990ec68e9e2c9d87b4313767fbf4f6612a63
Daruny Salazar Cardoza <dasalaza@cbr12s2.42barcelona.com>
2023-10-01 23:16:11 +0200

    all ok

--- ft_put_uint_nbr.c
+++ ft_put_uint_nbr.c
@@ -6,7 +6,7 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 12:58:13 by dasalaza          #+#    #+#             */
-/*   Updated: 2023/10/01 20:24:13 by dasalaza         ###   ########.fr       */
+/*   Updated: 2023/10/01 22:25:42 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,12 @@
 void	ft_put_uint_nbr(unsigned int nbr, int *counter_len)
 {
 	if (nbr >= 10)
+	{
 		ft_put_uint_nbr(nbr / 10, counter_len);
+		if (*counter_len == -1)
+			return ;
+	}
 	ft_putchar_n(nbr % 10 + '0', counter_len);
-	if (*counter_len < 0)
+	if (*counter_len == -1)
 		return ;
 }
--- main.c
+++ main.c
@@ -1,13 +1,23 @@
 
 #include "ft_printf.h"
+#include <unistd.h>
 
 int	main()
 {
-	int	x02;
+	//int	x02;
+	int	d02;
 	//x02 = -1;
-	x02 = 0;
+	//x02 = 0;
+	d02 = 10000;
 	//printf("test x\n");
-	ft_printf("%x\n", x02);
+	//ft_printf("%x\n", x02);
+	int fd = dup(STDOUT_FILENO);
+	close(STDOUT_FILENO);
+	int a = ft_printf("%d\n", d02);
+	printf("--------------\n");
+	printf("%d\n", d02);
+	dup2(fd, STDOUT_FILENO);
+	printf("asdasd: %d\n", a);
 
 	return (0);
 }
